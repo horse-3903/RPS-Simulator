a5e9a4f26257b907e3dc18ab9619e7e26b2c656e
horse-3903 <86842464+horse-3903@users.noreply.github.com>
2022-05-08 17:11:21 +0800

    update setup.py

--- RPS-3/src/setup.py
+++ RPS-3/src/setup.py
@@ -10,7 +10,7 @@ executables = [cx_Freeze.Executable("./RPS-3/src/rps-3_app.py", base=base)]
 
 cx_Freeze.setup(
     name = "RPS-3",
-    options = {"build_exe": {"packages":["tkinter","matplotlib","pandas","numpy","PIL"], "include_files":["./RPS-3/src/rock.png","./RPS-3/src/paper.png","./RPS-3/src/scissors.png"]}},
+    options = {"build_exe": {"packages":["tkinter","matplotlib","pandas","numpy","PIL"], "include_files":["./RPS-3/src/images/rock.png","./RPS-3/src/images/paper.png","./RPS-3/src/images/scissors.png"]}},
     version = "0.1",
     description = "RPS-3 Simulator",
     executables = executables
