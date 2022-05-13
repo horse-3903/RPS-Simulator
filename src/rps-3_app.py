d84bcb6c30c3f65f822a8e0cc71d25533f5e4770
horse-3903 <chongchoonhourafael@gmail.com>
2022-05-13 10:06:44 +0800

    update patch #10

--- src/rps-3_app.py
+++ src/rps-3_app.py
@@ -322,7 +322,7 @@ class RPS:
       self.update(2,"No. of Draws")
       self.wonplayerlabel.config(text="Draw")
 
-    # add conditions for rps-4 and rps-5
+    # add conditions for rps-5
       
     elif (p1 == 0 and p2 == 1) or (p1 == 1 and p2 == 2) or (p1 == 2 and p2 == 0) or (p1 == 1 and p2 == 3) or (p1 == 2 and p2 == 3) or (p1 == 3 and p2 == 0):
       # player 2 wins
