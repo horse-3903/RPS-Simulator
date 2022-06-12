5f3a91c56a8e49af25035549295d2464a3fd783a
horse-3903 <chongchoonhourafael@gmail.com>
2022-06-12 22:23:32 +0800

    update patch #13

--- src/setup.py
+++ src/setup.py
@@ -1,17 +1,9 @@
-import cx_Freeze
-import sys
-
-base = None
-
-if sys.platform == 'win32':
-    base = "Win32GUI"
-
-executables = [cx_Freeze.Executable("./src/rps-3_app.py", base=base)]
+import cx_Freeze,tkinter,checklistcombobox,numpy,pandas,PIL,os
 
 cx_Freeze.setup(
     name = "RPS-3",
-    options = {"build_exe": {"packages":["tkinter","matplotlib","pandas","numpy","PIL"], "include_files":[f"./src/images/{path}.png" for path in ["rock","paper","scissors","glue","pen"]]}},
+    options = {"build_exe": {"packages":["tkinter","matplotlib","pandas","numpy","PIL","os"], "include_files":[f"./src/{path}.png" for path in ["rock","paper","scissors","glue","pen"]]+["./src/checklistcombobox.py"]}},
     version = "0.1",
     description = "RPS-3 Simulator",
-    executables = executables
+    executables = [cx_Freeze.Executable("./src/RPS.py", base="Win32GUI")]
     )
